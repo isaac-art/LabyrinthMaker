5e78df5fd7a3f16acf8d3619c6c410cae7fe62dc
isaac clarke <isaac@ishimura.uk>
2018-07-24 13:41:04 +0100

    add glfw version better windowing support

---
+++ LabyrinthMakerGLFW.py
@@ -0,0 +1,180 @@
+import glfw
+import cv2
+import numpy as np
+from Mask import Mask
+from PIL import Image
+from OpenGL.GL import *
+from pseyepy import Camera
+from random import randint
+from datetime import datetime
+from MaskedGrid import MaskedGrid
+from RecursiveBacktracker import RecursiveBacktracker
+
+
+class LabyrinthMaker():
+    """LabyrinthMaker"""
+    def __init__(self):
+        self.start = datetime.now()
+        self.cap = Camera([0], fps=30, resolution=Camera.RES_LARGE, colour=True, auto_gain=True, auto_exposure=True, auto_whitebalance=True)
+        self.laby = []
+        self.l_bg = None
+        self.width = 1280
+        self.height = 720
+        self.l_average = 0
+        self.l_frame = None
+        self.grid = None
+        self.mask = None
+
+
+    def process_cam(self, sz, flip, bw=True):
+        # get the frame
+        frame, timestamp = self.cap.read()
+        # crop to correct ratio
+        # frame = frame[100:460, 0:640]
+        frame = frame[0:360, 0:640]
+        # -1 flip hori+vert / 1 flip vert / 0 flip hori
+        frame = cv2.flip(frame, flip)
+        # resize smaller for faster processing
+        # small = cv2.resize(frame, (0, 0), fx=0.15, fy=0.15)
+        small = cv2.resize(frame, (0, 0), fx=sz, fy=sz)
+        small = cv2.cvtColor(small, cv2.COLOR_RGB2BGR)
+        self.l_frame = small
+        if not bw:
+            return small
+        gray = cv2.cvtColor(small, cv2.COLOR_BGR2GRAY)
+        # threshold the image
+        # otsu threshold is adaptive
+        #   so will adjust to the range present in the image
+        ret, thr = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        # opening and dilating to remove noise
+        # kernel size is size of operation
+        kernel = np.ones((1, 1), np.uint8)
+        opening = cv2.morphologyEx(thr, cv2.MORPH_OPEN, kernel, iterations=1)
+        bg = cv2.dilate(opening, kernel, iterations=3)
+        return bg
+
+
+    def draw_cam(self):
+        # draws the camera to a second window
+        bg = self.process_cam(0.5, 1, bw=False)
+        cv2.namedWindow("camera")
+        cv2.moveWindow("camera", 0, 0)
+        cv2.imshow("camera", bg)
+
+
+    def draw_mask(self):   
+        if self.mask is not None:
+            # Blur the camera image 
+            self.l_frame = cv2.blur(self.l_frame, (10, 10))
+            glPointSize(13.333*2)     
+            glBegin(GL_POINTS)
+            for r in range(self.mask.rows):
+                for c in range(self.mask.columns):
+                    if not self.mask.cell_at(r, c):
+                        bb, gg, rr = self.l_frame[r, c]
+                        glColor3f(rr/255, gg/255, bb/255)
+                        # glVertex2f((c+0.5)*13.333, (r+0.5)*13.333)
+                        glVertex2f((c+0.5)*(13.333*2), (r+0.5)*(13.333*2))
+
+                        # bb, gg, rr = self.l_frame[r+1, c+1]
+                        # glColor3f(rr/255, gg/255, bb/255)
+                        # glVertex2f(c*13.333+12, r*13.333+12)
+
+            glEnd()
+
+
+    def draw_laby(self):
+        # Draws the labyrinth to gl
+        # set the line width for drawing
+        glLineWidth(2)
+        # set the color of the line
+        glColor3f(0.1, 0.1, 0.1)
+        # begin shape with pairs of lines
+        glBegin(GL_LINES)
+        # the list of points is backwards so reverse it
+        # self.mz.reverse()
+        # loop over coordinates adding all the vertices
+        for loc in self.laby:
+            x1, y1, x2, y2 = loc
+            # @ 0.1  = *5
+            # @ 0.25 = *2
+            # @ 0.15 = *3.333 
+            # glVertex2f(x1*3.333, y1*3.333)
+            # glVertex2f(x2*3.333, y2*3.333)
+            glVertex2f(x1*6.666, y1*6.666)
+            glVertex2f(x2*6.666, y2*6.666)
+            # glVertex2f(x1*4, y1*4)
+            # glVertex2f(x2*4, y2*4)
+        # complete the shape and draw everything
+        glEnd()
+
+
+    def refresh_scene(self):
+        # refresh the gl scene
+        glViewport(0, 0, self.width*2, self.height*2)
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        glOrtho(0.0, self.width*2, 0.0, self.height*2, 0.0, 1.0)
+        glMatrixMode (GL_MODELVIEW)
+        glLoadIdentity()
+
+
+    def update(self):
+        # update the labyrinth from camera image
+        bg = self.process_cam(0.15, -1)
+        # if not first frame
+        if self.l_bg is not None:
+            # calculate the average of the current bg
+            average = np.average(bg)
+            # compare to the last stored average
+            diff = average - self.l_average
+            # if there is a big enough difference +/-
+            if diff > 1.5 or diff < -1.5:
+                # translate numpy array to PIL image
+                pil_im = Image.fromarray(bg)
+                # make a mask from the image
+                self.mask = Mask.from_img_data(pil_im)
+                # build a grid from the unmasked areas
+                self.grid = MaskedGrid(self.mask)
+                # build walls in the grid
+                RecursiveBacktracker.on(self.grid)
+                # get walls as list of coordinate pairs for drawing
+                self.laby = self.grid.to_point_pairs(cell_size=4)
+            # save the new average
+            self.l_average = average
+        # save the background
+        self.l_bg = bg
+
+
+    def draw(self):
+        glClearColor(1, 1, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        glLoadIdentity()
+        self.refresh_scene()
+        self.update()
+        self.draw_mask()
+        self.draw_laby()
+        # self.draw_cam()
+
+
+    def main(self):
+        if not glfw.init():
+            return
+        # http://www.glfw.org/docs/latest/monitor_guide.html#monitor_monitors
+        # monitor = glfw.get_primary_monitor()
+        # mode = monitor.video_mode
+        window = glfw.create_window(self.width, self.height, "LabyrinthMaker_GLFW", None, None)
+        if not window:
+            glfw.terminate()
+            return
+        glfw.make_context_current(window)
+        while not glfw.window_should_close(window):
+            # render
+            self.draw()
+            glfw.swap_buffers(window)
+            glfw.poll_events()
+        glfw.terminate()
+
+
+labyrinth = LabyrinthMaker()
+labyrinth.main()
